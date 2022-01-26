95d02428407cd59b518771d5e5eee9ebfbef6a1d
Ching Pei Yang <badnam3o.0@gmail.com>
2022-01-26 19:44:47 +0100

    main page: added basic link to view_drive of referred drive

--- page_view_main.py
+++ page_view_main.py
@@ -11,8 +11,9 @@ def carSharer():
     freePlace = []
     fahrtCost = []
     fahrtStatus = []
+    fid=[]
     curs = conn.cursor()
-    curs.execute("SELECT transportmittel, startort, zielort, maxplaetze, fahrtkosten, status FROM fahrt")
+    curs.execute("SELECT transportmittel, startort, zielort, maxplaetze, fahrtkosten, status, fid FROM fahrt")
     data = curs.fetchall()
     for row in data:
         fahrtType.append(row[0])
@@ -21,22 +22,26 @@ def carSharer():
         freePlace.append(row[3])
         fahrtCost.append(row[4])
         fahrtStatus.append(row[5])
+        fid.append(row[6])
     totalfahrt = len(fahrtStart)
 
     fahrtType1 = []
     fahrtStart1 = []
     fahrtEnd1 = []
     fahrtStatus1 = []
+    fid1 = []
     curs1 = conn.cursor()
-    curs1.execute("SELECT transportmittel, startort, zielort, status FROM fahrt f, reservieren r WHERE r.kunde = 1 AND f.fid = r.fahrt")
+    curs1.execute("SELECT transportmittel, startort, zielort, status, fid FROM fahrt f, reservieren r WHERE r.kunde = 1 AND f.fid = r.fahrt")
     data1 = curs1.fetchall()
     for row1 in data1:
         fahrtType1.append(row1[0])
         fahrtStart1.append(row1[1])
         fahrtEnd1.append(row1[2])
         fahrtStatus1.append(row1[3])
+        fid1.append(row1[4])
     totalfahrt1 = len(fahrtStart1)
+    print(f'fid {fid}\n fid1 {fid1}')
     return render_template('carSharer.html', fahrtType=fahrtType, fahrtStart=fahrtStart, fahrtEnd=fahrtEnd,
     freePlace=freePlace, fahrtCost=fahrtCost, fahrtStatus=fahrtStatus, totalfahrt=totalfahrt,
     fahrtType1=fahrtType1, fahrtStart1=fahrtStart1, fahrtEnd1=fahrtEnd1,
-    fahrtStatus1=fahrtStatus1, totalfahrt1=totalfahrt1)
+    fahrtStatus1=fahrtStatus1, totalfahrt1=totalfahrt1, fid=fid, fid1=fid1)
