41cae8b5b34105a23ff2f773279c8b50e5345909
Ching Pei Yang <badnam3o.0@gmail.com>
2022-01-27 21:16:11 +0100

    Search page now working

--- page_view_search.py
+++ page_view_search.py
@@ -1,22 +1,30 @@
-from flask import render_template
+from flask import render_template, request
 import connect
 
 def carSearch():
     conn = connect.DBUtil().getExternalConnection()
+    start = request.args.get('start', None, str)
+    destination = request.args.get('destination', None, str)
+    fromDate = request.args.get('fromdate', None, str)
 
-    #input from user value1, value2, value3 to be initialized
+    if start is None or destination is None or fromDate is None:
+        return render_template('view_search.html', totalfahrt=0)
 
     fahrtType = []
     fahrtStart = []
     fahrtEnd = []
     fahrtCost = []
+    fahrtId = []
     curs = conn.cursor()
-    curs.execute("SELECT transportmittel, startort, zielort, fahrtkosten FROM fahrt WHERE startort LIKE ('value1%') AND zielort LIKE ('value2%') AND fahrtdatumzeit >= value3 AND status LIKE ('offen') ")
+    sql = "SELECT transportmittel, startort, zielort, fahrtkosten, fid FROM fahrt WHERE startort LIKE ('%%%s%%') AND zielort LIKE ('%%%s%%') AND fahrtdatumzeit >= '%s' AND status='offen' "
+    curs.execute(sql % (start, destination, fromDate))
     data = curs.fetchall()
     for row in data:
         fahrtType.append(row[0])
         fahrtStart.append(row[1])
         fahrtEnd.append(row[2])
         fahrtCost.append(row[3])
+        fahrtId.append(row[4])
     totalfahrt = len(fahrtStart)
-    return render_template('view_search.html', fahrtType=fahrtType, fahrtStart=fahrtStart, fahrtEnd=fahrtEnd, fahrtCost=fahrtCost, totalfahrt=totalfahrt)
+    return render_template('view_search.html', fahrtType=fahrtType, fahrtStart=fahrtStart, fahrtEnd=fahrtEnd,
+                           fahrtCost=fahrtCost, fahrtId=fahrtId, totalfahrt=totalfahrt)
